eb41fc39fe5b5602b4c67b3be830d16860eb73d5
Julien Kittel <julien.kittel@proton.me>
2025-05-28 21:10:06 +0200

    Work for deployment
    - separated config files by environment
    - added queries cache for sqlx offline support
    - added Dockerfile and docker-compose

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,18 +1,57 @@
 use config::Config;
-
+use serde_aux::field_attributes::deserialize_number_from_string;
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings
 }
 
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
     pub password: String,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
+}
+
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub port: u16,
+    pub host: String
+}
+
+enum Environment {
+    Local,
+    Production
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match &self {
+            Environment::Local => "local",
+            Environment::Production => "production"
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Environment::Local),
+            "production" => Ok(Environment::Production),
+            other => {
+                Err(format!(
+                    "invalid \"{}\" environment variable, must be either 'local' or 'production'",
+                    other
+                ))
+            }
+        }
+    }
 }
 
 impl DatabaseSettings {
@@ -22,7 +61,9 @@ impl DatabaseSettings {
             self.username, self.password, self.host, self.port, self.database_name
         )
     }
-    
+
+    // allow(dead_code) because it is used in tests.
+    #[allow(dead_code)]
     pub fn connection_string_without_db(&self) -> String {
         format!(
             "postgres://{}:{}@{}:{}",
@@ -32,9 +73,20 @@ impl DatabaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let settings = Config::builder()
-        .add_source(config::File::with_name("configuration.yaml"))
-        .build()?;
+    let base_path = std::env::current_dir().expect("Failed to get current directory.");
+    let configuration_directory = base_path.join("configuration");
 
-    settings.try_deserialize()
+    let settings = Config::builder()
+        .add_source(config::File::with_name(configuration_directory.join("base.yml").as_path().to_str().unwrap()).required(true));
+
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+    
+    settings.add_source(config::File::with_name(configuration_directory.join(environment.as_str()).as_path().to_str().unwrap()).required(true))
+        .add_source(config::Environment::with_prefix("APP").separator("__"))
+        .build()?
+        .try_deserialize()
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 mod routes;
 mod startup;
 use crate::configuration::get_configuration;
-use sqlx::PgPool;
 use std::net::TcpListener;
 mod configuration;
 use learning::telemetry::{get_subscriber, init_subscriber};
+use sqlx::postgres::PgPoolOptions;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("learning".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let db_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
+    let db_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string())
         .expect("Failed to connect to Postgres database");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host ,configuration.application.port
+    );
     let listener = TcpListener::bind(address).expect("Failed to bind 8080 port.");
     startup::run(listener, db_pool)?.await
 }
