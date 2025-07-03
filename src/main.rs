1e7003d9222d5b8899b0774cbe53af91f58ccf94
Julien Kittel <julien.kittel@proton.me>
2025-07-03 15:13:47 +0200

    feat: add opentelemetry support with existing tracing for jaeger

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use learning::configuration::get_configuration;
 use learning::startup::Application;
-use learning::telemetry::{get_subscriber, init_subscriber};
+use learning::telemetry::{init_telemetry};
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
-    let subscriber = get_subscriber("learning".into(), "info".into(), std::io::stdout);
-    init_subscriber(subscriber);
+    
+    let app_name = std::env::var("CARGO_PKG_NAME").unwrap_or("learning api".into());
+
+    init_telemetry(&app_name, &"learning-api-tracer".to_string())?;
+    
     let configuration = get_configuration().expect("Failed to read configuration.");
 
     let application = Application::build(configuration).await?;
--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,8 +1,15 @@
 use actix_web::error::BlockingError;
+use opentelemetry::trace::TracerProvider;
+use opentelemetry::{KeyValue, global};
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::Resource;
+use opentelemetry_sdk::trace::{RandomIdGenerator, SdkTracerProvider};
+use std::time::Duration;
 use tracing::{Subscriber, subscriber::set_global_default};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::fmt::MakeWriter;
+use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 
 pub fn get_subscriber<Sink>(
@@ -34,4 +41,42 @@ where
 {
     let current_span = tracing::Span::current();
     actix_web::web::block(move || current_span.in_scope(f))
+}
+
+pub fn init_telemetry(app_name: &String, tracer_name: &String) -> Result<(), anyhow::Error> {
+    let otlp_exporter = opentelemetry_otlp::SpanExporter::builder()
+        .with_tonic()
+        .with_endpoint("http://localhost:4317")
+        .with_timeout(Duration::from_secs(3))
+        .build()?;
+
+    let provider = SdkTracerProvider::builder()
+        .with_simple_exporter(otlp_exporter)
+        .with_id_generator(RandomIdGenerator::default())
+        .with_resource(
+            Resource::builder_empty()
+                .with_attributes([KeyValue::new("service.name", app_name.clone())])
+                .build(),
+        )
+        .build();
+    
+    global::set_tracer_provider(provider.clone());
+
+    let tracer = provider.tracer(tracer_name.clone());
+
+    let otel_layer = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_target(false)
+        .compact();
+
+    let filter_layer = EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("info"))?;
+
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer)
+        .with(otel_layer)
+        .init();
+
+    Ok(())
 }
